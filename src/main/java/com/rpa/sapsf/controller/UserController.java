c0cd07e3126f06c9d16dcafffdc803ae4fdf4775
Guruswamy-CK <chinna-krishnaia.gari-guruswamy@capgemini.com>
2024-10-16 13:28:38 +0530

    Test
    Signed-off-by: Guruswamy-CK <chinna-krishnaia.gari-guruswamy@capgemini.com>

--- src/main/java/com/rpa/sapsf/controller/UserController.java
+++ src/main/java/com/rpa/sapsf/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.rpa.sapsf.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +36,7 @@ public class UserController {
 		}
 		return response;
 	}
-	
+
 	@PostMapping("/user-forgot-password")
 	public UserDTO userForgotPassword(@RequestBody UserDTO user) {
 		UserDTO response = new UserDTO();
@@ -45,5 +46,11 @@ public class UserController {
 			e.printStackTrace();
 		}
 		return response;
+	}
+
+	@GetMapping("/say-hello")
+	public String sayHello() {
+
+		return "Hello";
 	}
 }
